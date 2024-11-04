59d98f7f324e07c69a54155c13afdaea9b790203
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2024-11-04 18:56:38 +0500

    bug fix

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ require("dotenv").config()
 const express = require('express');
 // const {randomUUID} = require('crypto');
 const app = express();
-const db=require('./database/db')
+// const db=require('./database/db')
 const cors = require('cors')
-db()
+// db()
 
 app.use(express.json())
 app.use(cors());
 app.use(express.urlencoded({ extended: true })); 
 
  
-// ***************** USER ***************** //
-app.use('/api', require('./routes/User'))
 app.get('/', (req,res)=>res.send("up and running"))
+// // ***************** USER ***************** //
+// app.use('/api', require('./routes/User'))
 
-// Task
-app.use('/api', require('./routes/Questions'));
+// // Task
+// app.use('/api', require('./routes/Questions'));
 
-// Point 
-app.use('/api', require('./routes/Point'))
-app.use('/api/widthdraw', require('./routes/Withdraw'))
+// // Point 
+// app.use('/api', require('./routes/Point'))
+// app.use('/api/widthdraw', require('./routes/Withdraw'))
 
 
-app.use('/uploads', express.static('uploads'));
-app.use('/api', require('./routes/User_Approve'))
+// app.use('/uploads', express.static('uploads'));
+// app.use('/api', require('./routes/User_Approve'))
 
 // console.log(crypto.randomUUID());
 app.listen(process.env.PORT, () => {
