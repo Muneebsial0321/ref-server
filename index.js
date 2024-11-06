030a747a3abd186aaba1fd5a03d87c74f11705fd
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2024-11-06 18:31:51 +0500

    bug fixed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true })); 
 
  
-app.get('/', (req,res)=>res.send(`up and running ${process.env.MONGO_URL}`))
+app.get('/', (req,res)=>res.send(`up and running `))
 app.get('/users', (req,res)=>res.json({data:"we are users"}))
 // // ***************** USER ***************** //
 app.use('/api', require('./routes/User'))
